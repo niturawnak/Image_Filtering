4106eeccf72321356f8e6ccc455322ec44ebb882
Mahmuda Rawnak Jahan <32084346+niturawnak@users.noreply.github.com>
2022-12-14 00:49:15 +0100

    Add files via upload

---
+++ build/CMakeFiles/HelloWorld_Exec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld_Exec.dir/src/main.cpp.o"
+  "HelloWorld_Exec"
+  "HelloWorld_Exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld_Exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
